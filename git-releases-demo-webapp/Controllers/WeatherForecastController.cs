5e99ae92ab97a40d8570d7b603a8384c70ed5842
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-11-30 14:16:39 +0100

    feat: modify max value temperature

--- git-releases-demo-webapp/Controllers/WeatherForecastController.cs
+++ git-releases-demo-webapp/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ public class WeatherForecastController : ControllerBase
         return Enumerable.Range(1, 8).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
+            TemperatureC = Random.Shared.Next(-20, 95),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
