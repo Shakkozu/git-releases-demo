8dc7ae76d8ccd6276a14055e658bba4c589e5485
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-11-30 12:55:45 +0100

    feat: Add paging to get larger weather forecasts set endpoint

--- git-releases-demo-webapp/Controllers/WeatherForecastController.cs
+++ git-releases-demo-webapp/Controllers/WeatherForecastController.cs
@@ -31,14 +31,17 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet("larger-set", Name = "Get Large Set Weather Forecast")]
-    public IEnumerable<WeatherForecast> GetLargerSet()
+    public IEnumerable<WeatherForecast> GetLargerSet(int pageSize, int page)
     {
+
         return Enumerable.Range(1, 50).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
+            .Skip(pageSize * page - 1)
+            .Take(pageSize)
         .ToArray();
     }
 }
