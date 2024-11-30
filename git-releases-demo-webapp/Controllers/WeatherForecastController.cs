d70308a8d51ff1db2766d908df0be341711b0075
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-11-30 14:08:12 +0100

    feature: bla bla

--- git-releases-demo-webapp/Controllers/WeatherForecastController.cs
+++ git-releases-demo-webapp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "test"
     };
 
     [HttpGet(Name = "GetWeatherForecast")]
